2945499be787e2520d28fab42d05aa7156cf0f55
PeterWaIIace <walas.piotr.prywatny@gmail.com>
2024-05-15 00:09:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,8 @@ class MyMainWindow(QMainWindow):
         main_layout.addWidget(left_frame, stretch=0)
 
         # Add a label to left frame (optional)
-        label_left = QLabel("Pilot")
+        label_left = QLabel("EyePilot")
+        label_left.setStyleSheet("color: white; font-size: 40px;")
         label_left.setAlignment(Qt.AlignCenter)
         left_layout = QVBoxLayout()
         left_frame.setLayout(left_layout)
@@ -48,13 +49,34 @@ class MyMainWindow(QMainWindow):
         button1 = QPushButton("Calibration")
         button2 = QPushButton("Settings")
         button3 = QPushButton("Some more text")
+
+        button_style = """
+            QPushButton {
+                color: Black;
+                font-size: 20px;
+                background-color: rgba(255, 255, 255, 0.0);
+                width: 200px;
+            }
+
+            QPushButton:hover {
+                background: qradialgradient(cx:0.5, cy:0.5, radius:0.5, fx:0.5, fy:0.5, stop:0 rgba(255, 255, 255, 0.5), stop:1 rgba(255, 255, 255, 0.3), stop:2 rgba(255, 255, 2, 0.0));
+                border-radius: 5px;
+            }
+        """
+
+        button1.setStyleSheet(button_style)
+        button2.setStyleSheet(button_style)
+        button3.setStyleSheet(button_style)
+
         right_layout = QVBoxLayout()
         right_layout.setAlignment(Qt.AlignCenter)
-        right_frame.setStyleSheet("background-color: rgba(255, 255, 255, 0.1);")
+        right_frame.setStyleSheet("background-color: rgba(255, 255, 255, 0.2);")
         right_frame.setLayout(right_layout)
         right_layout.addWidget(button1)
         right_layout.addWidget(button2)
         right_layout.addWidget(button3)
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
